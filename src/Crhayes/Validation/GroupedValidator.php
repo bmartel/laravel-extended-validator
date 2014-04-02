bfadf2993a6ee91be07b39a4d9335ff8e76e5374
BrandonMartel <b.martel@tippingcanoe.com>
2014-04-02 11:04:54 -0500

    added a setter for BatchValidator in GroupedValidator

--- src/Crhayes/Validation/GroupedValidator.php
+++ src/Crhayes/Validation/GroupedValidator.php
@@ -5,8 +5,8 @@ namespace Crhayes\Validation;
 use Crhayes\Validation\Exceptions\MissingValidatorException;
 use Crhayes\Validation\Exceptions\ValidatorContextException;
 
-class GroupedValidator
-{
+class GroupedValidator {
+
 	/**
 	 * An array of Validator objects we will spin through
 	 * when running our grouped validation.
@@ -22,43 +22,45 @@ class GroupedValidator
 	 */
 	private $errors = [];
 
-    /**
-     * @var BatchValidator
-     */
-    protected $batchValidator;
+	/**
+	 * @var BatchValidator
+	 */
+	protected $batchValidator;
 
 	/**
 	 * Create a new GroupedValidator, with the option of specifying
 	 * either a single validator object or an array of validators.
 	 *
-	 * @param mixed 	$validator
+	 * @param mixed $validator
 	 */
-	public function __construct(BatchValidator $batchValidator = null,$validator = [])
-	{
-        $this->batchValidator = $batchValidator;
+	public function __construct(BatchValidator $batchValidator = null, $validator = []) {
 
-		if ($validator) $this->addValidator($validator);
+		$this->batchValidator = $batchValidator;
+
+		if ($validator) {
+			$this->addValidator($validator);
+		}
 	}
 
 	/**
 	 * Static shorthand for creating a new grouped validator.
 	 *
-	 * @param  mixed 	$validator
+	 * @param  mixed $validator
 	 * @return \Crhayes\Validation\GroupedValidator
 	 */
-	public static function make(BatchValidator $batchValidator = null,$validator = [])
-	{
-		return new static($batchValidator,$validator);
+	public static function make(BatchValidator $batchValidator = null, $validator = []) {
+
+		return new static($batchValidator, $validator);
 	}
 
 	/**
 	 * Add a validator to spin through. Accepts either a single
 	 * Validator object or an array of validators.
 	 *
-	 * @param mixed 	$validator
+	 * @param mixed $validator
 	 */
-	public function addValidator($validator)
-	{
+	public function addValidator($validator) {
+
 		$validator = is_array($validator) ? $validator : [$validator];
 
 		$this->validators = array_merge($this->validators, $validator);
@@ -66,42 +68,42 @@ class GroupedValidator
 		return $this;
 	}
 
-    /**
-     * Performs a batch validation for data requiring the same validation
-     *
-     * @param array $dataCollection
-     * @param ContextualValidator $validator
-     * @param null $context
-     * @return $this
-     */
-    public function batchValidate(array $dataCollection, ContextualValidator $validator, \Closure $callable = null)
-    {
-        if(empty($this->batchValidator)) throw new MissingValidatorException;
+	/**
+	 * Performs a batch validation for data requiring the same validation
+	 *
+	 * @param array $dataCollection
+	 * @param ContextualValidator $validator
+	 * @param null $context
+	 * @return $this
+	 */
+	public function batchValidate(array $dataCollection, ContextualValidator $validator, \Closure $callable = null) {
 
-        $this->batchValidator->process($dataCollection,$validator);
+		if (empty($this->batchValidator))
+			throw new MissingValidatorException;
 
-        if(is_callable($callable)){
-            call_user_func($callable,$this->batchValidator);
-        }
+		$this->batchValidator->process($dataCollection, $validator);
 
-        $this->addValidator($this->batchValidator->getValidators());
+		if (is_callable($callable)) {
+			call_user_func($callable, $this->batchValidator);
+		}
 
-        return $this;
-    }
+		$this->addValidator($this->batchValidator->getValidators());
+
+		return $this;
+	}
 
 	/**
 	 * Perform a check to see if all of the validators have passed.
 	 *
 	 * @return boolean
 	 */
-	public function passes()
-	{
-		if ( ! count($this->validators)) throw new MissingValidatorException('No validators provided: You must provide at least one validator');
+	public function passes() {
 
-		foreach ($this->validators as $validator)
-        {
-			if ( ! $validator->passes())
-			{
+		if (!count($this->validators))
+			throw new MissingValidatorException('No validators provided: You must provide at least one validator');
+
+		foreach ($this->validators as $validator) {
+			if (!$validator->passes()) {
 				$this->errors += $validator->getMessageBag()->getMessages();
 			}
 		}
@@ -109,32 +111,43 @@ class GroupedValidator
 		return (count($this->errors)) ? false : true;
 	}
 
-    /**
-     * Get the registered validators
-     *
-     * @return array
-     */
-    public function getValidators()
-    {
-        return $this->validators;
-    }
+	/**
+	 * Sets the GroupValidator to use another instance of BatchValidator
+	 *
+	 * @param BatchValidator $batchValidator
+	 */
+	public function setBatchValidator(BatchValidator $batchValidator) {
+
+		$this->batchValidator = $batchValidator;
+	}
+
+	/**
+	 * Get the registered validators
+	 *
+	 * @return array
+	 */
+	public function getValidators() {
+
+		return $this->validators;
+	}
+
 	/**
 	 * Perform a check to see if any of the validators have failed.
 	 *
 	 * @return boolean
 	 */
-	 public function fails()
-	 {
-	 	return ! $this->passes();
-	 }
+	public function fails() {
+
+		return !$this->passes();
+	}
 
 	/**
 	 * Return the combined errors from all validators.
 	 *
 	 * @return \Illuminate\Support\MessageBag
 	 */
-	public function errors()
-	{
+	public function errors() {
+
 		return new \Illuminate\Support\MessageBag($this->errors);
 	}
 }
